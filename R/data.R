800a61ffc53a624e3bc73306d355395dc960267c
Carlos Scheidegger <cscheid@research.att.com>
2013-02-28 18:21:45 -0500

    tags, trees

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 # blobs
-get.blob <- function(ctx, owner, repo, sha) api.get.request(ctx, c("repos", owner, repo, "git", "blobs", sha))
+get.blob    <- function(ctx, owner, repo, sha)     api.get.request(ctx, c("repos", owner, repo, "git", "blobs", sha))
 create.blob <- function(ctx, owner, repo, content) api.post.request(ctx, c("repos", owner, repo, "git", "blobs"), body=content)
 
 # commits
-get.commit <- function(ctx, owner, repo, sha) api.get.request(ctx, c("repos", owner, repo, "git", "commits", sha))
+get.commit    <- function(ctx, owner, repo, sha)     api.get.request(ctx, c("repos", owner, repo, "git", "commits", sha))
 create.commit <- function(ctx, owner, repo, content) api.post.request(ctx, c("repos", owner, repo, "git", "commits", body=content))
 
 # references
@@ -20,3 +20,10 @@ delete.reference <- function(ctx, owner, repo, ref) api.delete.request(ctx, c("r
 
 # tags
 get.tag <- function(ctx, owner, repo, sha) api.get.request(ctx, c("repos", owner, repo, "git", "tags", sha))
+create.tag <- function(ctx, owner, repo, content) api.post.request(ctx, c("repos", owner, repo, "git", "tags"), expect_code=201, body=content)
+
+# trees
+get.tree <- function(ctx, owner, repo, sha, ...) api.get.request(ctx, c("repos", owner, repo, "git", "trees", sha), params=.rest(...))
+create.tree <- function(ctx, owner, repo, content) api.post.request(ctx, c("repos", owner, repo, "git", "trees"), expect_code=201, body=content)
+
+
