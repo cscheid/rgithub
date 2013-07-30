6ce16bcd44d684bce80a14325499145f6f71f56c
Gordon Woodhull <gordon@woodhull.com>
2013-07-30 11:36:23 -0400

    always return list(ok, content, code)

--- R/github.R
+++ R/github.R
@@ -28,11 +28,11 @@ rgithub.context.from.token <- function(url, client_id, client_secret, access_tok
 {
   ctx <- list(token=access_token, api_url=url, client_secret=client_secret, token=access_token, client_id=client_id, etags=new.env(parent=emptyenv()))
   r <- get.myself(ctx)
-  if(r$succeeded) {
-    ctx$user <- content(r$response)
-    list(succeeded = TRUE, content = ctx)
+  if(r$ok) {
+    ctx$user <- r$content
+    list(ok = TRUE, content = ctx)
   }
-  else list(succeeded = FALSE, content = content(r$response))
+  else list(ok = FALSE, content = content(r$response))
 }
 
 build.url <- function(ctx, req, params)
@@ -62,7 +62,7 @@ api.request <- function(ctx, req, method, expect.code=200, params=list(), config
   #fix for http://developer.github.com/changes/2013-04-24-user-agent-required/
   config<-c(config, user_agent(getOption("HTTPUserAgent")))
   r <- method(url, config=config)
-  list(succeeded = r$status_code %in% expect.code, response = r);
+  list(ok = r$status_code %in% expect.code, content = content(r), code = r$status_code);
 }
 
 # body can either be a json object (an R list of the right type), a length-1 character, or NULL
@@ -80,7 +80,7 @@ api.request.with.body <- function(ctx, req, method, expect.code=200, params=list
   #fix for http://developer.github.com/changes/2013-04-24-user-agent-required/
   config<-c(config, user_agent(getOption("HTTPUserAgent")))
   r = method(url, config=config, body=body)
-  list(succeeded = r$status_code %in% expect.code, response = r);
+  list(ok = r$status_code %in% expect.code, content = content(r), code = r$status_code);
 }
 
 api.get.request    <- function(ctx, req, expect.code=200, params=list(), config=accept_json()) api.request(ctx, req, GET, expect.code, params, config)
@@ -93,8 +93,8 @@ api.test.request <- function(ctx, path)
 {
   r=api.get.request(ctx, path, expect.code=c(204, 404))
 
-  if(r$succeeded)
-    list(succeeded = TRUE, content = r$status == 204)
+  if(r$ok)
+    list(ok = TRUE, content = r$code == 204)
   else
-    list(succeeded = FALSE, content = content(r$response))
+    list(ok = FALSE, content = content(r$response))
 }
