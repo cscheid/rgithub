5c7cf211be6d58ca08266ff54bdcf14b7b4aac18
Carlos Scheidegger <cscheid@research.att.com>
2013-11-04 12:43:55 -0500

    update test

--- inst/tests/test_basic.r
+++ inst/tests/test_basic.r
@@ -2,6 +2,6 @@ context("Basic Tests")
 
 test_that("A basic rgithub context can be acquired", {
   ctx <- create.github.context("https://api.github.com")
-  repos <- get.user.repositories(ctx, "cscheid")
+  repos <- get.user.repositories("cscheid", ctx = ctx)
   print(repos)
 })
