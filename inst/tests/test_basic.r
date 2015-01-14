4e7080424edb47e553373c0f17c2735610f094a4
jennybc <jenny@stat.ubc.ca>
2015-01-13 16:52:56 -0800

    whitespace

--- inst/tests/test_basic.r
+++ inst/tests/test_basic.r
@@ -1,13 +1,13 @@
 context("Basic Tests")
 
 test_that("A basic rgithub context can be acquired", {
-	create.github.context("https://api.github.com")
-	repos <- get.user.repositories("cscheid")
-	repos_overview <- do.call("rbind",
-														lapply(repos$content[1:5], function(x) {
-															data.frame(name = x$name,
-																				 owner = x$owner$login,
-																				 updated_at = x$updated_at)}))
-	cat("\n")
-	print(repos_overview)
+  create.github.context("https://api.github.com")
+  repos <- get.user.repositories("cscheid")
+  repos_overview <- do.call("rbind",
+                            lapply(repos$content[1:5], function(x) {
+                              data.frame(name = x$name,
+                                         owner = x$owner$login,
+                                         updated_at = x$updated_at)}))
+  cat("\n")
+  print(repos_overview)
 })
